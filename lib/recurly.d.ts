a0ec77051ba3de970f850c2b4916a21e60458c5f
DX Codefresh <dx@recurly.com>
2022-06-29 16:22:43 +0000

    Generated Latest Changes for v2019-10-10

--- lib/recurly.d.ts
+++ lib/recurly.d.ts
@@ -1438,6 +1438,10 @@ export declare class LineItem {
    */
   unitAmount?: number | null;
   /**
+   * Determines whether or not tax is included in the unit amount. The Tax Inclusive Pricing feature (separate from the Mixed Tax Pricing feature) must be enabled to utilize this flag.
+   */
+  taxInclusive?: boolean | null;
+  /**
    * `quantity * unit_amount`
    */
   subtotal?: number | null;
@@ -1647,6 +1651,10 @@ export declare class Subscription {
    * Subscription unit price
    */
   unitAmount?: number | null;
+  /**
+   * Determines whether or not tax is included in the unit amount. The Tax Inclusive Pricing feature (separate from the Mixed Tax Pricing feature) must be enabled to utilize this flag.
+   */
+  taxInclusive?: boolean | null;
   /**
    * Subscription quantity
    */
